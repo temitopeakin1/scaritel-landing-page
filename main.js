961771852c39725dd7936f824976a399f97cf706
Temitope Akinmegha <temitopeakinmegha@gmail.com>
2020-07-18 18:44:46 +0100

    added Js scripts

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+var count = new Date("July 22,2020 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var d = count - now;
+
+    var days =  Math.floor(d/(1000*60*60*24));
+    var hours = Math.floor((d%(1000*60*60*60))/(1000*60*60));
+    var minutes = Math.floor((d%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((d%(1000*60))/1000);
+
+    document.getElementById("days").innerHTML = days; 
+    document.getElementById("hours").innerHTML = hours; 
+    document.getElementById("minutes").innerHTML = minutes; 
+    document.getElementById("seconds").innerHTML = seconds; 
+
+    if(d <= 0) {
+        clearInterval(x);
+    }
+
+},1000);
